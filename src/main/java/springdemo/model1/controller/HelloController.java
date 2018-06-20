176a494e212e74cb6d50c27564c9d35c807c2cc7
sunnysen <bug11@126.com>
2018-06-20 18:07:52 +0800

    thymeleaf each list data

--- src/main/java/springdemo/model1/controller/HelloController.java
+++ src/main/java/springdemo/model1/controller/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * @author Sunnysen
@@ -15,7 +18,8 @@ public class HelloController {
 
     @RequestMapping("/")
     public String hello(@RequestParam(name = "name",defaultValue = "world") String username,  Model model){
-        model.addAttribute("message","Hello,"+username);
+        List<String> messages = Arrays.asList(username+"-message1",username+"-message2");
+        model.addAttribute("messages",messages);
         return "resultPage";
     }
 }
