43b6de79e4cc077ef8d828dab8fb21dacd7dd219
sunnysen <bug11@126.com>
2018-06-20 17:24:07 +0800

    thymeleaf message from the controller and url param

--- src/main/java/springdemo/model1/controller/HelloController.java
+++ src/main/java/springdemo/model1/controller/HelloController.java
@@ -3,6 +3,7 @@ package springdemo.model1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -13,8 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello(Model model){
-        model.addAttribute("message","hello from the controller");
+    public String hello(@RequestParam("name") String username,  Model model){
+        model.addAttribute("message","Hello,"+username);
         return "resultPage";
     }
 }
