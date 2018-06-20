63535bd9dc69dcc38ee5b1b84879257ce4de4153
sunnysen <bug11@126.com>
2018-06-20 15:55:34 +0800

    init

---
+++ src/main/java/springdemo/model1/web/HelloController.java
@@ -0,0 +1,23 @@
+package springdemo.model1.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 特殊@Component
+ * 一般结合@RequestMapping使用
+ */
+@Controller
+/**
+ *
+ * @author Daniel
+ */
+public class HelloController {
+
+    @RequestMapping
+    @ResponseBody
+    public String hello(){
+
+    }
+}
