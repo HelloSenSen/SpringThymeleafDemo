ba6dd95b76c90cc72e2a8316742ae85a43b81323
sunnysen <bug11@126.com>
2018-06-20 15:58:39 +0800

    move web to controller

--- src/main/java/springdemo/model1/controller/HelloController.java
+++ src/main/java/springdemo/model1/controller/HelloController.java
@@ -1,4 +1,4 @@
-package springdemo.model1.web;
+package springdemo.model1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
